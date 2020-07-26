2d06ab818d46972e6fd72eccf2015b56b468308b
shitagon@walla.com <danik270694>
2020-07-26 17:21:14 +0300

    BattHurt Client

--- app/src/main/java/com/danik/batthurt/MainActivity.java
+++ app/src/main/java/com/danik/batthurt/MainActivity.java
@@ -6,9 +6,12 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -45,6 +48,18 @@ public class MainActivity extends AppCompatActivity {
         myChargeRef=database.getReference("CurrentIsCharging_"+Build.MODEL);
         if(myRef.getKey().length() >0)
             fbhURL.setText(R.string.firebase_database_url);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + packageName));
+                startActivity(intent);
+            }
+        }
+
         mainLoop();
     }
 
